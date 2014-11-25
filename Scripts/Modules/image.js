0d8552bbe460b6118e0e555b1852237e5b88a4e5
fireliangbin <451061218@qq.com>
2014-11-25 16:49:23 +0800

    cg
    cg

---
+++ Scripts/Modules/image.js
@@ -0,0 +1,53 @@
+﻿define(function (require, exports) {
+    var $ = require("jquery");
+
+
+    var Action = {
+
+        Init: function () {
+
+
+            $(".divOverlay").offset({ top: $(".smallImg").offset().top, left: $(".smallImg").offset().left + $(".smallImg").width() + 10 });
+            $(".smallImg").hover(function () {
+                $(".spZoom").show();
+                $(".divOverlay").show();
+            }, function () {
+                $(".spZoom").hide();
+                $(".divOverlay").hide();
+            }).mousemove(function (e) {
+                var xWidth = $(this).width();
+                var yHegiht = $(this).height();
+                var x = $(this).offset().left;
+                var y = $(this).offset().top;
+                var left = e.pageX - $(".spZoom").width() / 2;
+                var top = e.pageY - $(".spZoom").height() / 2;
+
+                if (left < x) {
+                    left = x;
+                }
+                else if ((e.pageX + $(".spZoom").width() / 2) > (x + xWidth)) {
+                    left = x + xWidth - $(".spZoom").width();
+                }
+                if (top < y) {
+                    top = y;
+                }
+                else if ((e.pageY + $(".spZoom").height() / 2) > (y + yHegiht)) {
+                    top = y + yHegiht - $(".spZoom").height();
+                }
+                $(".spZoom").offset({ left: left, top: top });
+                console.log($(".baseImg").width())
+
+                $(".baseImg").offset({ left: $(".divOverlay").offset().left - (left - x) * $(".baseImg").width() / xWidth, top: $(".divOverlay").offset().top - (top - y) * $(".baseImg").height() / yHegiht });
+
+
+
+
+            })
+        }
+
+
+    }
+
+
+    exports.Action = Action;
+})
